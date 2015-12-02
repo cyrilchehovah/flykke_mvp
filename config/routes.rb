75c1e90d7a499a4f97669d4865dce9d0d108435a
David <davi.legault@gmail.com>
2015-12-02 18:52:17 +0100

    Adding facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
   post 'new_preview', to: 'posts#new_preview', as: 'new_preview'

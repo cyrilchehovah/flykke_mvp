9684fe9c849d55154eaaea0d79e19e0ea9f7eb83
Cyril Chehovah <c.chehovah@gmail.com>
2015-12-03 20:36:16 +0100

    some change in design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
     search_string = params[:post][:url]
     @page = MetaInspector.new(search_string)
     @category = params[:category]
-    @post = Post.new(title: @page.best_title, category: @category, description: @page.description)
+    @post = Post.new(title: @page.best_title, category: @category, description: @page.description, image: @page.images.best, url: @page.url, site_name: @page.meta['og:site_name'])
     # render new_post_path
   end
 
